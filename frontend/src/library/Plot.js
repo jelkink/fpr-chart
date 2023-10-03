796b34bf4d5002163dbdb4269c9e4d999e1e3c45
Jos Elkink <jelkink@gmail.com>
2023-10-03 14:23:44 +0100

    Cosmetic changes

--- frontend/src/library/Plot.js
+++ frontend/src/library/Plot.js
@@ -1,10 +1,10 @@
-import Chart from "chart.js/auto";
-import { tabulate, tabulate_bivariate, pair, bin } from "./Tabulate";
-import { linearRegression, minimum, maximum } from "./Stats";
+import Chart from "chart.js/auto"
+import { tabulate, tabulate_bivariate, pair, bin } from "./Tabulate"
+import { linearRegression, minimum, maximum } from "./Stats"
 
 const singleBarChart = function(var1) {
 
-    const table = tabulate(var1);
+    const table = tabulate(var1)
 
     return({
         labels: Object.keys(table),
@@ -15,22 +15,22 @@ const singleBarChart = function(var1) {
         }],
         backgroundColor: "blue",
         borderWidth: 1,
-    });
+    })
 }
 
 const bivariateBarChart = function(var1, var2) {
 
-    var tables = tabulate_bivariate(var1, var2);
+    var tables = tabulate_bivariate(var1, var2)
 
     return({
         labels: Object.keys(tabulate(var1)),
         datasets: tables
-    });
+    })
 }
 
 const histogram = function(var1, bins) {
 
-    const table = bin(var1, bins);
+    const table = bin(var1, bins)
 
     return({
         labels: Object.keys(table),
@@ -41,7 +41,7 @@ const histogram = function(var1, bins) {
         }],
         backgroundColor: "blue",
         borderWidth: 1,
-    });
+    })
 }
 
 const scatterPlot = function(var1, var2, jitter, jitter_sd, regression = false) {
@@ -77,50 +77,64 @@ const scatterPlot = function(var1, var2, jitter, jitter_sd, regression = false) 
 class Plot {
 
     constructor() {
-        this.chart = null;
+        this.chart = null
     }
 
     update(data, selectedGraph, selectedVar1, selectedVar2, jitter, jitter_sd, bins, regression) {
 
-        const ctx = document.getElementById("chart");
+        const ctx = document.getElementById("chart")
 
-        var var1 = null;
-        var var2 = null;
-        var title = '';
+        var var1 = null
+        var var2 = null
+        var title = ''
+        var labelX = ''
+        var labelY = ''
+        var showLegend = true
 
         if (data) {
-            var1 = (data.hasVariable(selectedVar1) ? data.getVariable(selectedVar1) : null);
-            var2 = (data.hasVariable(selectedVar2) ? data.getVariable(selectedVar2) : null);
+            var1 = (data.hasVariable(selectedVar1) ? data.getVariable(selectedVar1) : null)
+            var2 = (data.hasVariable(selectedVar2) ? data.getVariable(selectedVar2) : null)
         }
 
         if (selectedGraph) {
 
-            const newGraph = this.chart === null || this.chart.type !== selectedGraph;
+            const newGraph = this.chart === null || this.chart.type !== selectedGraph
 
-            var data = null;
+            var data = null
 
             if (selectedGraph === "bar" & var1 !== null) {
 
                 if (var2 === null || selectedVar1 == selectedVar2) {
-                    data = singleBarChart(var1);
-                    title = var1.label;
+                    data = singleBarChart(var1)
+                    title = "Bar chart of " + var1.label
+                    labelX = var1.label
+                    labelY = "Frequency"
+                    showLegend = false
                 } else {
-                    data = bivariateBarChart(var1, var2);
-                    title = var2.label + " by " + var1.label;
+                    data = bivariateBarChart(var1, var2)
+                    title = "Bar chart of " + var2.label + " by " + var1.label
+                    labelX = var1.label
+                    labelY = "Frequency"
                 }
-            };
+            }
 
             if (selectedGraph === "histogram" & var1 !== null) {
 
-                data = histogram(var1, bins);
-                title = "Histogram of " + var1.label;
-            };
+                data = histogram(var1, bins)
+                title = "Histogram of " + var1.label
+                labelX = var1.label
+                labelY = "Frequency"
+                showLegend = false
+            }
 
             if (selectedGraph === "scatter" & var1 !== null & var2 !== null) {
 
-                data = scatterPlot(var1, var2, jitter, jitter_sd, regression);
-                title = var1.label + " by " + var2.label;
-            };
+                data = scatterPlot(var1, var2, jitter, jitter_sd, regression)
+                title = "Scatter plot of " + var1.label + " by " + var2.label
+                labelX = var2.label
+                labelY = var1.label
+                showLegend = false
+            }
 
             if (data !== null) {
 
@@ -135,33 +149,44 @@ class Plot {
                             },
                             legend: {
                                 position: "bottom",
-                            },
-                            scales: {
-                                y: {
-                                    ticks: {
-                                        beginAtZero: true,
-                                    }
-                                }
+                                display: showLegend
                             }
                         },
+                        scales: {
+                            y: {
+                                title: {
+                                    text: labelY,
+                                    display: true
+                                },
+                                ticks: {
+                                    beginAtZero: true,
+                                }
+                            },
+                            x: {
+                                title: {
+                                    text: labelX,
+                                    display: true
+                                }
+                            }
+                        }
                     },
-                };
-
-                if (newGraph) {
-                    if (this.chart !== null) this.chart.destroy();
-                    this.chart = new Chart(ctx, config);
-                } else {
-                    this.chart.data = data;
-                    this.chart.type = (selectedGraph === "histogram" ? "bar" : selectedGraph);
-                    this.chart.options = config.options;
-                    this.chart.update();
                 }
 
-                console.log(data);
-                console.log(config);
+                if (newGraph) {
+                    if (this.chart !== null) this.chart.destroy()
+                    this.chart = new Chart(ctx, config)
+                } else {
+                    this.chart.data = data
+                    this.chart.type = (selectedGraph === "histogram" ? "bar" : selectedGraph)
+                    this.chart.options = config.options
+                    this.chart.update()
+                }
+
+                console.log(data)
+                console.log(config)
             }
         }
-    };
-};
+    }
+}
 
-export default Plot;
+export default Plot
